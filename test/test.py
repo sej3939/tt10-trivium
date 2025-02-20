0e118e1905033435606a35ad97db86f849743c6e
sej3939 <sj3939@nyu.edu>
2025-02-20 18:11:44 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,9 +1,13 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
 
+import random
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
+
+import trivium_uisyudha
 
 @cocotb.test()
 async def test_project(dut):
@@ -17,24 +21,30 @@ async def test_project(dut):
     dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
-    dut.rst_n.value = 0
-    dut.key.value = 0x9719CFC92A9FF688F9AA
-    dut.iv.value = 0xECBB76B09AFF71D0D151
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
-    await ClockCycles(dut.clk, 3)
+    
     # Set the input values you want to test
-
+    key = 0x9719CFC92A9FF688F9AA
+    iv = 0xECBB76B09AFF71D0D151
     # Wait for clock cycles to see the output values
     keystream = ""
-    for i in range(0,80):
+    for i in range(0,1000):
+        dut.rst_n.value = 0
+        dut.key.value = key
+        dut.iv.value = iv
+        trivium_inst = trivium(key,iv)
+        key = (key << 1) | random.randint(0, 1)
+        IV = (IV << 1) | random.randint(0, 1)
         await ClockCycles(dut.clk, 1)
-        keystream += str(dut.keystream_bit.value)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
+        dut.rst_n.value = 1
+        await ClockCycles(dut.clk, 3)
+        
+        for j in range(0,80):
+            await ClockCycles(dut.clk, 1)
+            keystream += str(dut.keystream_bit.value)
+        assert int(keystream, 2) == int("".join(map(trivium_inst.keystream(80), bits)), 2)
+        
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
