5ec9ae784fc867ffa874372e31f9fafe820f6484
sej3939 <sj3939@nyu.edu>
2025-02-20 13:38:40 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -13,7 +13,7 @@ module tt_um_example (
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire       rst_n,     // reset_n - low to reset
     // EXTRA IO FOR TESTING ONLY (take out for final)
     input reg key[79:0],
     input reg iv[79:0],
