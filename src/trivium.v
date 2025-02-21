de1c392579aafe6cd02c6d77e19839d523cb7bc4
sej3939 <sj3939@nyu.edu>
2025-02-21 16:24:32 -0500

    Update trivium.v

--- src/trivium.v
+++ src/trivium.v
@@ -19,19 +19,18 @@ module trivium (
     // Initialization Phase
     always @(posedge clk or negedge rst) begin
         if (!rst) begin
-            // Reset all registers
-            s = 288'b0;
+            // Load key into s
+            s[287:208] <= key[79:0];
+            s[207:193] <= 0;
+            // Load IV into s
+            s[194:115] <= iv[79:0];
+            s[114:3] <= 0;
+            // Set the last 3 bits of s to 1 as per Trivium spec
+            s[2:0] <= 3'b111;
             initialized <= 0;
         end
         else if (!initialized) begin
-            // Load key into s
-            s[287:208] = key[79:0];
-
-            // Load IV into s
-            s[194:115] = iv[79:0];
-
-            // Set the last 3 bits of s to 1 as per Trivium spec
-            s[2:0] = 3'b111;
+            
 
             // Shift register state is cycled 4 full times (4*288=1152)
             for (i = 0; i < 1151; i = i + 1) begin
@@ -40,9 +39,9 @@ module trivium (
                 t2 = s[126] ^ s[111] ^ (s[112] & s[113]) ^ s[24];
                 t3 = s[45] ^ s[0] ^ (s[2] & s[1]) ^ s[219];
                 // Shift registers and insert feedback
-                s[287:195] = {t3, s[287:196]};
-                s[194:111] = {t1, s[194:112]};
-                s[110:0] = {t2, s[110:1]};
+                s[287:195] <= {t3, s[287:196]};
+                s[194:111] <= {t1, s[194:112]};
+                s[110:0] <= {t2, s[110:1]};
             end
             initialized <= 1;
         end
@@ -65,9 +64,9 @@ module trivium (
             t3 = t3 ^ (s[2] & s[1]) ^ s[219];
 
             // Shift registers and insert feedback
-            s[287:195] = {t3, s[287:196]};
-            s[194:111] = {t1, s[194:112]};
-            s[110:0] = {t2, s[110:1]};
+            s[287:195] <= {t3, s[287:196]};
+            s[194:111] <= {t1, s[194:112]};
+            s[110:0] <= {t2, s[110:1]};
         end
     end
 
