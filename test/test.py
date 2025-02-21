093cea2b215bb340b1b3baac098c36b5490cdb8b
sej3939 <sj3939@nyu.edu>
2025-02-21 14:24:48 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -51,7 +51,7 @@ async def test_project(dut):
         
         for j in range(0,80):
             await ClockCycles(dut.clk, 1)
-            keystream += str(dut.keystream_bit.value)
+            keystream += str(dut.uo_out[0].value)
         actual_keystream = trivium_inst.keystream(80)
         dut._log.info(f"keystream: {hex(int(keystream, 2))[2:].upper().zfill(20)}, actual keystream: {hex(bin_list_to_int_bitwise(actual_keystream))[2:].upper().zfill(20)}")
         assert int(keystream, 2) == bin_list_to_int_bitwise(actual_keystream)
