27a4920cd5b0489743c91933b008c0708f7efda1
sej3939 <sj3939@nyu.edu>
2025-03-04 17:12:35 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -47,7 +47,7 @@ async def test_project(dut):
         #iv = ((iv << 1) | random.randint(0, 1)) & ((1 << 80) - 1)
         await ClockCycles(dut.clk, 1)
         dut.rst_n.value = 1
-        await ClockCycles(dut.clk, 1152)
+        await ClockCycles(dut.clk, 1153)
         
         for j in range(0,80):
             await ClockCycles(dut.clk, 1)
