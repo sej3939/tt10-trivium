e89cbcac854598d2f5d747da428f8c42b6b13aae
sej3939 <sj3939@nyu.edu>
2025-03-04 21:09:36 -0500

    Update trivium.v

--- src/trivium.v
+++ src/trivium.v
@@ -53,7 +53,7 @@ module trivium (
 
             // initialize counter
             i <= i + 1;
-            if (i == 1153) begin
+            if (i == 1151) begin
                 initialized <= 1;
             end
         end
